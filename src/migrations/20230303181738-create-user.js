e49d8c0030c9c658aa59cab2278e54cddda83abc
Scarlat Pereira <engcivil.scarlat@gmail.com>
2023-03-03 15:43:54 -0300

    feat: add req 01 - migration users

---
+++ src/migrations/20230303181738-create-user.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable(
+      'Users',
+       { 
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        displpayName: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          field: 'display_name',
+        },
+        email: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          unique: true,
+        },
+        password: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+        },
+        image: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+      });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+   await queryInterface.dropTable('Users');
+  },
+};
