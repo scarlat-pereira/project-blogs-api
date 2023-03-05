f6d546ff3333e1f5de28a4688a5833d59db9ac6d
Scarlat Pereira <engcivil.scarlat@gmail.com>
2023-03-05 18:22:33 -0300

    feat: add req07 create category (models)

---
+++ src/models/Category.js
@@ -0,0 +1,19 @@
+const CategoryModel = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'categories',
+    underscored: true,
+  });
+
+    return Category;
+};
+
+module.exports = CategoryModel;
